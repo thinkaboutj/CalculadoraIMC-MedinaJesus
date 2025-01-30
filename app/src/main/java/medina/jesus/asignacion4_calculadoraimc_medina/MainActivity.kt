3ae4eb77ce31dcd9919da8e780f4c24a4592ae83
Jesus <159750556+thinkaboutj@users.noreply.github.com>
2025-01-30 11:52:33 -0700

    ahi va casi la interfaz

---
+++ app/src/main/java/medina/jesus/asignacion4_calculadoraimc_medina/MainActivity.kt
@@ -0,0 +1,20 @@
+package medina.jesus.asignacion4_calculadoraimc_medina
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+
+    //variables
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //evento boton click o clicklistener
+    }
+
+    //funcion
+}
